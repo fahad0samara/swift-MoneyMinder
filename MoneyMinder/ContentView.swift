90e28af535bad079bb47434004806d15e5eb0887
fahad0samarar <fah8ad@outlook.com>
2024-02-26 09:25:35 +0200

    Initial Commit

---
+++ MoneyMinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoneyMinder
+//
+//  Created by fahad samara on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoneyMinder/MoneyMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyMinderApp.swift
+//  MoneyMinder
+//
+//  Created by fahad samara on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MoneyMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
