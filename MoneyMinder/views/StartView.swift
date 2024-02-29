d462c06c63c7a6b73d68ab0859c69b8aeed358ed
fahad0samarar <fah8ad@outlook.com>
2024-02-29 11:57:44 +0200

    add  screen for the satart and the tabview

---
+++ MoneyMinder/views/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  MoneyMinder
+//
+//  Created by fahad samara on 2/29/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
---
+++ MoneyMinder/views/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  MoneyMinder
+//
+//  Created by fahad samara on 2/29/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
